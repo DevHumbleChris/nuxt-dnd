dc390f7867a5c5c43991b78ce2aebd8398c9a338
The Coding Montana <63234437+DevHumbleChris@users.noreply.github.com>
2023-09-12 08:41:14 +0000

    added taskie dashbaord

--- components/taskie/Sidebar.vue
+++ components/taskie/Sidebar.vue
@@ -1,74 +1,85 @@
 <script setup>
-const data = user()
+const data = user();
 
 const image = computed(() => {
-    return data?.image
-})
-
+  return data?.image;
+});
 </script>
 
 <template>
-    <aside class="sticky w-[20rem] h-screen bg-white p-5 space-y-8">
-        <div class="flex items-center gap-2">
-            <div class="w-8 h-8 border-2 rounded-full">
-                <nuxt-img :src="image" class="w-full h-full object-cover rounded-full"/>
-            </div>
-            <p class="text-sm">DevHumbleChris</p>
+  <aside class="sticky w-[20rem] h-screen bg-white p-5 space-y-8">
+    <div class="flex items-center gap-2">
+      <div class="w-8 h-8 border-2 rounded-full">
+        <nuxt-img
+          :src="image"
+          class="w-full h-full object-cover rounded-full"
+        />
+      </div>
+      <p class="text-sm">DevHumbleChris</p>
+    </div>
+    <ul>
+      <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
+        <NuxtLink to="/taskie/dashboard" class="flex items-center gap-2">
+          <Icon name="ph:user-bold" class="w-5 h-auto" />
+          <p>Dashboard</p>
+        </NuxtLink>
+      </li>
+    </ul>
+    <div class="space-y-4">
+      <div class="flex items-center justify-between text-gray-400">
+        <p class="text-sm">Your Projects</p>
+        <button>
+          <Icon name="ion:plus-round" />
+        </button>
+      </div>
+      <ul class="text-sm">
+        <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
+          <NuxtLink to="#" class="flex items-center gap-2">
+            <Icon name="ph:user-bold" class="w-5 h-auto" />
+            <p>Personal</p>
+          </NuxtLink>
+        </li>
+        <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
+          <NuxtLink to="#" class="flex items-center gap-2">
+            <Icon name="ph:user-bold" class="w-5 h-auto" />
+            <p>Personal</p>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div class="space-y-4">
+      <div class="flex items-center justify-between text-gray-400">
+        <div>
+          <div></div>
+          <p class="text-sm">Private Projects</p>
         </div>
-        <div class="space-y-4">
-            <div class="flex items-center justify-between text-gray-400">
-                <p class="text-sm">Your Projects</p>
-                <button>
-                    <Icon name="ion:plus-round" />
-                </button>
-            </div>
-            <ul class="text-sm">
-                <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
-                    <NuxtLink to="#" class="flex items-center gap-2">
-                        <Icon name="ph:user-bold" class="w-5 h-auto"/>
-                        <p>Personal</p>
-                    </NuxtLink>
-                </li>
-                <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
-                    <NuxtLink to="#" class="flex items-center gap-2">
-                        <Icon name="ph:user-bold" class="w-5 h-auto"/>
-                        <p>Personal</p>
-                    </NuxtLink>
-                </li>
-            </ul>
-        </div>
-        <div class="space-y-4">
-            <div class="flex items-center justify-between text-gray-400">
-                <div>
-                    <div>
-
-                    </div>
-                    <p class="text-sm">Private Projects</p>
-                </div>
-                <button>
-                    <Icon name="ion:plus-round" />
-                </button>
-            </div>
-            <ul class="text-sm">
-                <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
-                    <NuxtLink to="#" class="flex items-center gap-2">
-                        <Icon name="ph:user-bold" class="w-5 h-auto"/>
-                        <p>Personal</p>
-                    </NuxtLink>
-                </li>
-                <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
-                    <NuxtLink to="#" class="flex items-center gap-2">
-                        <Icon name="ph:user-bold" class="w-5 h-auto"/>
-                        <p>Personal</p>
-                    </NuxtLink>
-                </li>
-            </ul>
-        </div>
-        <div class="absolute bottom-0 right-0 left-0">
-            <NuxtLink to="#" class="flex items-center gap-2 px-5 py-3 hover:bg-[#f5f6f8]">
-                <Icon name="charm:sign-out" class="w-5 h-auto" />
-                <p>Logout</p>
-            </NuxtLink>
-        </div>
-    </aside>
+        <button>
+          <Icon name="ion:plus-round" />
+        </button>
+      </div>
+      <ul class="text-sm">
+        <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
+          <NuxtLink to="#" class="flex items-center gap-2">
+            <Icon name="ph:user-bold" class="w-5 h-auto" />
+            <p>Personal</p>
+          </NuxtLink>
+        </li>
+        <li class="hover:bg-[#f5f6f8] p-2 rounded-md">
+          <NuxtLink to="#" class="flex items-center gap-2">
+            <Icon name="ph:user-bold" class="w-5 h-auto" />
+            <p>Personal</p>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div class="absolute bottom-0 right-0 left-0">
+      <NuxtLink
+        to="#"
+        class="flex items-center gap-2 px-5 py-3 hover:bg-[#f5f6f8]"
+      >
+        <Icon name="charm:sign-out" class="w-5 h-auto" />
+        <p>Logout</p>
+      </NuxtLink>
+    </div>
+  </aside>
 </template>
---
+++ pages/taskie/dashboard/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LeftSidebar from '~/components/taskie/dashboard/LeftSidebar.vue';
+</script>
+
+<template>
+    <section class="flex">
+        <div>hello</div>
+        <LeftSidebar />
+    </section>
+</template>
